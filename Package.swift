7c9ef8aa8a0fb3cd30db84d70176c07a70f992cc
release <release@local>
2026-09-24 09:34:52 +0800

    release 4.3.0

--- Package.swift
+++ Package.swift
@@ -9,13 +9,13 @@ let package = Package(
                  targets: ["AtomicXCoreBinary", "AtomicXCoreDeps"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Hanpto/RTCRoomEngine_SwiftPM.git", from: "4.3.1")
+        .package(url: "https://github.com/Hanpto/RTCRoomEngine_SwiftPM.git", from: "4.3.5")
     ],
     targets: [
         .binaryTarget(
             name: "AtomicXCoreBinary",
-            url: "https://ios-sdk-release-1300962221.cos.ap-guangzhou.myqcloud.com/ios/AtomicXCore/4.3.1/AtomicXCore.xcframework.zip",
-            checksum: "cd9f6f08e16df62d7d66303efb9f2ba5794baab9e025c2d9ae0b98ddb96c54a6"
+            url: "https://ios-sdk-release-1300962221.cos.ap-guangzhou.myqcloud.com/ios/AtomicXCore/4.3.0/AtomicXCore.xcframework.zip",
+            checksum: "f60a5c8f0e1d1329cb7a9b66a8bd95e40181850042f775e3f467eddf668f65ee"
         ),
         .target(
             name: "AtomicXCoreDeps",
